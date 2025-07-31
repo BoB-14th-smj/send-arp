4b7414991b320f1d30756e7caf1ed935e8236fbb
seo <hey_minj@naver.com>
2025-07-31 10:41:54 -0400

    change ether_type to e_type

--- ethernet.h
+++ ethernet.h
@@ -9,15 +9,12 @@ struct Ethernet {
 private:
     uint8_t d_mac_[6];
     uint8_t s_mac_[6];
-    uint16_t ether_type;
+    uint16_t e_type;
 
 public:
     uint8_t* get_d_mac(void) { return d_mac_; }
     uint8_t* get_s_mac(void) { return s_mac_; }
-    uint16_t get_ether_type(void) { return ntohs(ether_type); }
+    uint16_t get_ether_type(void) { return ntohs(e_type); }
 
 };
 
-
-void print_ethernet_header(Ethernet* ethernet);
-Ethernet* get_ethernet_header(const u_char* packet);
