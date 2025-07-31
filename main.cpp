ee7a6a1442519a25a41dfe31ab70d0b6cb690478
seo <hey_minj@naver.com>
2025-07-31 02:14:51 -0400

    open pcap

--- main.cpp
+++ main.cpp
@@ -1,23 +1,23 @@
 #include <cstdio>
+#include <cstdlib>
 #include <pcap.h>
-#include "ethhdr.h"
-#include "arphdr.h"
+#include <stdlib.h>
 
-#pragma pack(push, 1)
-struct EthArpPacket final {
-	EthHdr eth_;
-	ArpHdr arp_;
-};
-#pragma pack(pop)
 
 void usage() {
 	printf("syntax: send-arp-test <interface>\n");
 	printf("sample: send-arp-test wlan0\n");
 }
+int check_arg(int argc, char*argv){
+	if(argc != 2){
+		usage();
+		return EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
+}
 
 int main(int argc, char* argv[]) {
-	if (argc != 2) {
-		usage();
+	if(check_arg(argc, *argv)){
 		return EXIT_FAILURE;
 	}
 
@@ -29,26 +29,9 @@ int main(int argc, char* argv[]) {
 		return EXIT_FAILURE;
 	}
 
-	EthArpPacket packet;
 
-	packet.eth_.dmac_ = Mac("00:00:00:00:00:00");
-	packet.eth_.smac_ = Mac("00:00:00:00:00:00");
-	packet.eth_.type_ = htons(EthHdr::Arp);
 
-	packet.arp_.hrd_ = htons(ArpHdr::ETHER);
-	packet.arp_.pro_ = htons(EthHdr::Ip4);
-	packet.arp_.hln_ = Mac::Size;
-	packet.arp_.pln_ = Ip::Size;
-	packet.arp_.op_ = htons(ArpHdr::Request);
-	packet.arp_.smac_ = Mac("00:00:00:00:00:00");
-	packet.arp_.sip_ = htonl(Ip("0.0.0.0"));
-	packet.arp_.tmac_ = Mac("00:00:00:00:00:00");
-	packet.arp_.tip_ = htonl(Ip("0.0.0.0"));
-
-	int res = pcap_sendpacket(pcap, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
-	if (res != 0) {
-		fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(pcap));
-	}
+	printf("gh");
 
 	pcap_close(pcap);
 }
